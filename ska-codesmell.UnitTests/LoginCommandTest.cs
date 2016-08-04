5b9dbea14370d7c2001c1bc319e75042bd8d0e48
Kiang Kuang <kiangkuang@gmail.com>
2016-08-04 16:35:03 +0800

    Redesign LoginCommandTest

--- ska-codesmell.UnitTests/LoginCommandTest.cs
+++ ska-codesmell.UnitTests/LoginCommandTest.cs
@@ -3,21 +3,33 @@ using NUnit.Framework;
 
 namespace ska_codesmell.UnitTests
 {
+    [TestFixture]
+    public class LoginCommandTest
+    {
+        [TestCase("alice", "p@ssw0rd")]
+        [TestCase("bab", "cardinals")]
+        public void TestLoginCommandWriter(string name, string password)
+        {
+            StringWriter expectedWriter = new StringWriter();
 
-	[TestFixture] public class LoginCommandTest 
-	{
-		[Test] public void SentCorrectly() {                   
-			char [] knownGood = new char [] { (char)0xde, (char)0xad, (char)50, (char)48, (char)0x01,
-                                    'b', 'a', 'b', (char)0x00,
-                                    'c', 'a', 'r', 'd', 'i', 'n', 'a', 'l', 's', (char)0x00,
-                                    (char)0xbe, (char)0xef };
-			LoginCommand cmd = new LoginCommand("bab", "cardinals");
-			StringWriter writer = new StringWriter();
-			cmd.Write(writer);
+            expectedWriter.Write(new char[] { (char)0xde, (char)0xad });
+            expectedWriter.Write(name.Length + password.Length + 8);
+            expectedWriter.Write(new char[] { (char)0x01 });
+            expectedWriter.Write(name);
+            expectedWriter.Write((char)0x00);
+            expectedWriter.Write(password);
+            expectedWriter.Write(new char[] { (char)0x00, (char)0xbe, (char)0xef });
 
-			for( int i = 0; i < knownGood.Length; i++ ) {
-				Assert.AreEqual(knownGood[i], writer.ToString()[i],  "comparison failed at byte number " + i);
-			}
-		}
-	}
+            string expected = expectedWriter.ToString();
+
+            LoginCommand cmd = new LoginCommand(name, password);
+            StringWriter writer = new StringWriter();
+            cmd.Write(writer);
+
+            for (int i = 0; i < expected.Length; i++)
+            {
+                Assert.AreEqual(expected[i], writer.ToString()[i], "comparison failed at byte number " + i);
+            }
+        }
+    }
 }
