2007468edc1abb6c1605b5a59c911b7b72a800d2
Kiang Kuang <kiangkuang@gmail.com>
2016-08-04 12:49:49 +0800

    Rename AddEmployeeCmd variables to naming convention

--- ska-codesmell/AddEmployeeCmd.cs
+++ ska-codesmell/AddEmployeeCmd.cs
@@ -6,51 +6,51 @@ namespace GeneralizationCs
 
 	public class AddEmployeeCmd
 	{
-		private static char[] header = {(char)0xde, (char)0xad};
-		private static char[] commandChar = {(char)0x02};
-		private static char[] footer = {(char)0xbe, (char)0xef};
-		private const int SIZE_LENGTH = 1;
-		private const  int CMD_BYTE_LENGTH = 1;
-		private string name;
-		private string address;
-		private string city;
-		private string state;
-		private string yearlySalary;
+		private static readonly char[] Header = {(char)0xde, (char)0xad};
+		private static readonly char[] CommandChar = {(char)0x02};
+		private static readonly char[] Footer = {(char)0xbe, (char)0xef};
+		private const int SizeLength = 1;
+		private const  int CmdByteLength = 1;
+		private readonly string _name;
+		private readonly string _address;
+		private readonly string _city;
+		private readonly string _state;
+		private readonly string _yearlySalary;
 
 		public AddEmployeeCmd(string name, string address, string city, string state, int yearlySalary)
 		{
-			this.name = name;
-			this.address = address;
-			this.city = city;
-			this.state = state;
-			this.yearlySalary = yearlySalary + "";
+			this._name = name;
+			this._address = address;
+			this._city = city;
+			this._state = state;
+			this._yearlySalary = yearlySalary + "";
 		}
 
 		private int getSize() {
-			return header.Length +  SIZE_LENGTH +  CMD_BYTE_LENGTH + footer.Length +
-				name.Length + 1 +
-				address.Length + 1 +
-				city.Length + 1 +
-				state.Length + 1 +
-				yearlySalary.Length + 1;
+			return Header.Length +  SizeLength +  CmdByteLength + Footer.Length +
+				_name.Length + 1 +
+				_address.Length + 1 +
+				_city.Length + 1 +
+				_state.Length + 1 +
+				_yearlySalary.Length + 1;
 		}
 
 
 		public void Write(TextWriter writer) {
-			writer.Write(header);
+			writer.Write(Header);
 			writer.Write(getSize());
-			writer.Write(commandChar);
-			writer.Write(name);
+			writer.Write(CommandChar);
+			writer.Write(_name);
 			writer.Write((char)0x00);
-			writer.Write(address);
+			writer.Write(_address);
 			writer.Write((char)0x00);
-			writer.Write(city);
+			writer.Write(_city);
 			writer.Write((char)0x00);
-			writer.Write(state);
+			writer.Write(_state);
 			writer.Write((char)0x00);
-			writer.Write(yearlySalary);
+			writer.Write(_yearlySalary);
 			writer.Write((char)0x00);
-			writer.Write(footer);
+			writer.Write(Footer);
 		}
 	}
 }
